4fd8160f5646d46f17ae756829d45885d8ec0ea0
lynette hemingway <147886775+berryrice0@users.noreply.github.com>
2024-06-06 15:08:03 -0400

    Add files via upload

---
+++ src/pages/home/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import About from './sections/about/about';
+import Main from './sections/main/main';
+import './home.css'
+
+function Home() {
+    return (
+      <>
+        <Main />
+       
+        {/* {        
+        <About />
+        } */}
+
+      </>
+    );
+}
+
+export default Home;
---
+++ src/pages/home/sections/main/main.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import Typewriter from "typewriter-effect";
+
+import './main.css';
