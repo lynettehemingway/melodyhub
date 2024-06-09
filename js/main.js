14d665e806292f33afb9ec86e198170498bb7e95
berryrice0 <dolphinsquare03@gmail.com>
2024-06-08 23:47:15 -0400

    updates

---
+++ js/main.js
@@ -0,0 +1,33 @@
+//faq
+document.addEventListener('DOMContentLoaded',  () => {
+    const faqContainer = document.querySelector('.faq-content');
+
+    faqContainer.addEventListener('click', (e) => {
+        const groupHeader = e.target.closest('.faq-group-header');
+        
+        if(!groupHeader) return;
+
+        const group = groupHeader.parentElement;
+        const groupBody = group.querySelector('.faq-group-body');
+        const icon = groupHeader.querySelector('i');
+
+        // toggle icon
+        icon.classList.toggle('fa-plus');
+        icon.classList.toggle('fa-minus');
+
+        // toggle visibility of body
+        groupBody.classList.toggle('open');
+
+        // close other open faq bodies
+        const otherGroups = faqContainer.querySelectorAll('.faq-group');
+
+        otherGroups.forEach((otherGroup) => {
+            if(otherGroup != group){
+                const otherGroupBody = otherGroup.querySelector('.faq-group-body');
+                const otherIcon = otherGroup.querySelector('.faq-group-header i');
+
+                otherGroupBody.classList.remove('open');
+            }
+    });
+ });
+});
