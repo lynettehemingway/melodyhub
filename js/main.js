bd5a254e9b40a5d6bb4f5c10292bc30c27c0aa0e
berryrice0 <dolphinsquare03@gmail.com>
2024-07-04 13:33:49 -0400

    mobile-friendly

--- js/main.js
+++ js/main.js
@@ -31,3 +31,12 @@ document.addEventListener('DOMContentLoaded',  () => {
     });
  });
 });
+
+
+// mobile menu
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerButton = document.querySelector('.hamburger-button');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    hamburgerButton.addEventListener('click', () => mobileMenu.classList.toggle('active'));
+})
